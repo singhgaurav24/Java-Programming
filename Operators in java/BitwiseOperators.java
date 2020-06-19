5776c96d39a2c2e358d6c376f9cc9780572f09bd
gaurav <singhgaurav9698@gmail.com>
2020-06-19 21:31:45 +0530

    Operators in java

---
+++ Operators in java/BitwiseOperators.java
@@ -0,0 +1,14 @@
+
+public class BitwiseOperators {
+   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 4;
+		System.out.println(a&b);  //Bitwise AND operator
+        System.out.println(a|b);  //Bitwise OR operator
+        System.out.println(a^b);  //Bitwise XOR operator
+        System.out.println(~a);   //Compliment
+	}
+
+}
